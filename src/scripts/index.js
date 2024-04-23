969f2177f0439ecf802f560b8376ccb1952fb29e
João Pedro <jp871157@gmail.com>
2024-04-23 12:19:28 -0300

    feat: added feedback form and interest button on mobile view

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,5 +17,34 @@ const handleCloseNavbarNavigate = () => {
   })
 }
 
+const handleLabelAnimation = () => {
+  const fieldsets = document.querySelectorAll('fieldset')
+  // Adiciona classe que altera a propriedade TOP da label
+  fieldsets.forEach(fieldset => {
+    fieldset.children[1].addEventListener('focus', () => { fieldset.children[0].classList.add('valid') })
+  })
+  // Remove a classe que altera a propriedade TOP da label se o input estiver vazio
+  fieldsets.forEach(fieldset => {
+    console.log(fieldset.children[1].value);
+    fieldset.children[1].addEventListener('focusout', () => { if(fieldset.children[1].value.length === 0) { fieldset.children[0].classList.remove('valid') } })
+  })
+}
+
+const handleButtonDisabled = () => {
+  const buttonSubmit = document.querySelector('.feedback__form button')
+
+  buttonSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth()
+    const hour = date.getHours()
+
+    // Aqui virá a lógica para bloquear o botão até o fim da apresentação, aguardando data e horário!
+  })
+}
+
 handleMobileNavBar()
 handleCloseNavbarNavigate()
+handleLabelAnimation()
+handleButtonDisabled()
